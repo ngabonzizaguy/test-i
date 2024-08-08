1742899cd1dcf6dc39b7dde8e684b5861eb2fa37
N.Guy <32185091+ngabonzizaguy@users.noreply.github.com>
2024-08-08 10:31:03 +0000

    eslint rule disabled

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,15 @@
 module.exports = {
   root: true,
-  extends: ['plugin:@next/next/recommended', 'next', '@payloadcms'],
+  extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier', 'simple-import-sort'],
   rules: {
-    // 'prettier/prettier': ['error', { endOfLine: 'auto' }],
-    // 'simple-import-sort/imports': 'error',
-    // 'simple-import-sort/exports': 'error',
-    // 'react/no-unescaped-entities': 'off',
-    // '@next/next/no-page-custom-font': 'off',
-    // 'no-console': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
     'react/no-unescaped-entities': 'off',
     '@next/next/no-page-custom-font': 'off',
+    'no-console': 'off',
   },
   parserOptions: {
     sourceType: 'module',
